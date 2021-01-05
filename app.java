3e6d189df9eea85734378b60c9545ad3016f21ec
Ketu-Patel <ketupatel1010@gmail.com>
2021-01-05 08:38:16 -0600

    Added Http stuff

--- app.java
+++ app.java
@@ -1,10 +1,11 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.awt.event.ActionListener;
 import javax.swing.border.EmptyBorder;
 
-public class app extends JFrame {
+public class app extends JFrame implements ActionListener {
 
-    private JPanel contentPane;
 
     /**
      * Launch the application.
@@ -28,12 +29,24 @@ public class app extends JFrame {
      */
 
     public app() {
+        setTitle("Lol Dodge List");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 450, 300);
-        contentPane = new JPanel();
-        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-        contentPane.setLayout(new BorderLayout(0, 0));
-        JButton b = new JButton("Run");
-        setContentPane(contentPane);
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
+        JPanel p1 = new JPanel(new BorderLayout());
+        //contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        //contentPane.setLayout(new BorderLayout(0, 0));
+        JTextField name = new JTextField("Add Summoner Name");
+        name.setPreferredSize(new Dimension(100,100));
+        JButton runButton = new JButton("Run");
+        runButton.setPreferredSize(new Dimension(100,100));
+        p1.add(name);
+        contentPane.add(p1);
+    }
+
+    public void actionPerformed(ActionEvent e){
+
+
     }
 }
