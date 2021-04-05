62afb7ee3b1b2ca715b182db2be58632940a8219
Mani Nagarathnam <mnn3@illinois.edu>
2021-04-05 13:46:03 -0500

    Minor changes to GUI with inputs

--- GUI1.java
+++ GUI1.java
@@ -30,48 +30,55 @@ public class GUI1 {
             public void actionPerformed(ActionEvent e) {
                 //places data into arraylist
                 String s[] = placeLobbyChatHereTextArea.getText().split("\\r?\\n");
-                ArrayList<String> data =new ArrayList<>();
-                for(int i = 0; i< s.length; i++){
-                    String lines[] = s[i].split("\\s+");
-                    for(int j =0; j < lines.length;j++){
-                        data.add(lines[j]);
-                    }
-                }
-                //sift data and place in team
                 team = new ArrayList<>();
-                String name = "";
-                Boolean discard =false;
-                for(int i = 0; i< data.size(); i++){
-                    if(data.get(i).equals("joined") || data.get(i).equals("the")){
-                        continue;
-                    }
-                    else if(data.get(i).equals("left")){
-                        discard = true;
-                    }
-                    else if(data.get(i).equals("lobby")){
-                        System.out.println(team.size());
-                        if(team.size() == 4){
-                            i = data.size();
+                //For if a whole team is inputted
+                if (s.length > 1) {
+
+                    ArrayList<String> data = new ArrayList<>();
+                    for (int i = 0; i < s.length; i++) {
+                        String lines[] = s[i].split("\\s+");
+                        for (int j = 0; j < lines.length; j++) {
+                            data.add(lines[j]);
                         }
-                        if(discard){
-                            team.remove(name);
-                            discard=false;
-                        }
-                        else if(name.length() > 1){
-                            team.add(name);
-                        }
-                        name="";
                     }
-                    else{
-                        name = name +" "+ data.get(i);
+                    //sift data and place in team
+                    //TODO Filter out duplicate "has joined the lobby"
+
+                    String name = "";
+                    Boolean discard = false;
+                    for (int i = 0; i < data.size(); i++) {
+                        if (data.get(i).equals("joined") || data.get(i).equals("the")) {
+                            continue;
+                        } else if (data.get(i).equals("left")) {
+                            discard = true;
+                        } else if (data.get(i).equals("lobby")) {
+                            System.out.println(team.size());
+                            if (team.size() == 4) {
+                                i = data.size();
+                            }
+                            if (discard) {
+                                team.remove(name);
+                                discard = false;
+                            } else if (name.length() > 1) {
+                                team.add(name);
+                            }
+                            name = "";
+                        } else {
+                            name = name + " " + data.get(i);
+                        }
                     }
                 }
-                //System.out.println(team);
+
+                if (s.length == 1){
+                    team.add(s[0]);
+                }
+
                 //Check against DodgeList
                 DodgeEngine DodgeEngine2 = new DodgeEngine();
                 DodgeEngine2.ReadIn();
                 ArrayList<Player> found_inters = DodgeEngine2.FindPlayers(team);
                 dodgePlayerFound(found_inters);
+                System.out.println(found_inters.get(0).getName());
             }
         });
 
