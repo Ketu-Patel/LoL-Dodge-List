454919e9e8659311e532e6ea473e9afd6525f1af
Mani Nagarathnam <mnn3@illinois.edu>
2021-08-15 18:47:27 -0500

    Rewrote engine to add future functionality

--- application/DodgeEngine.java
+++ application/DodgeEngine.java
@@ -1,43 +1,38 @@
 package application;
 
-import org.jetbrains.annotations.NotNull;
 
 import java.util.*;
 import java.io.*;
 
 public class DodgeEngine {
-    ArrayList<Player> inters = new ArrayList<Player>();
+    Map<String, Player> playerMap;
 
     public DodgeEngine(){
 
     }
 
     /**
-     * Searches inters arraylist to find instances current team members
+     * Searches bad Players to find instances current team members
      * @param team
      * @return
      */
-    public ArrayList<Player> FindPlayers(@NotNull ArrayList<String> team){
-        ArrayList<Player> found_players = new ArrayList<Player>();
+    public Set<Player> FindPlayers(List<String> team){
+        Set<Player> foundPlayers = new HashSet<>();
         for (String teammate : team){
-            for (Player player : inters){
-                if (teammate.equals(player.getName())){
-                    found_players.add(player);
-                }
+            if(playerMap.containsKey(teammate)){
+                foundPlayers.add(playerMap.get(teammate));
             }
         }
-        return found_players;
+        return foundPlayers;
     }
 
     /**
-     * Stores Content of Inters Arraylist in Text File as CSV
+     * Stores Content of Map in Text File as CSV
      */
     public void StoreText(){
         try(FileWriter csvWriter = new FileWriter("application/Dodge_List.txt")) {
-            for (Player a : inters) {
-                String[] temp = {a.getName(), a.getMessage()};
-                csvWriter.append(String.join(",", temp));
-                csvWriter.append("\n");
+            for (Player a : playerMap.values()) {
+                csvWriter.append(a.toString() + "\n");
             }
             csvWriter.flush();
         }
@@ -48,19 +43,22 @@ public class DodgeEngine {
     }
 
     /**
-     * Reads in CSV to populate inters Arraylist
+     * Reads in CSV to populate Map of bad players
      */
     public void ReadIn(){
-        inters.clear();
         try( BufferedReader csvReader = new BufferedReader(new FileReader("application/Dodge_List.txt"))) {
             String row;
+            playerMap = new HashMap<String, Player>();
             while ((row = csvReader.readLine()) != null) {
-                String[] data = row.split(",");
-                AddPlayer(data[0], data[1]);
+                String[] split1 = row.split(", | =");
+                playerMap.put(split1[1], new Player(split1[1], Integer.parseInt(split1[3]), Integer.parseInt(split1[5]),
+                        Integer.parseInt(split1[7]), split1[9]));
+
             }
             csvReader.close();
         }
         catch (FileNotFoundException e){
+            playerMap = new HashMap<String, Player>();
             //You need to switch to an external error log
             System.out.println(e.getMessage());
         }
@@ -71,27 +69,26 @@ public class DodgeEngine {
     }
 
     public void AddPlayer(String name, String message){
-        inters.add(new Player(name, message));
+        if(!playerMap.containsKey(name)){
+            playerMap.put(name, new Player(name, message));
+        }
     }
 
     public void RemovePlayer(Player player){
-        inters.remove(player);
+        if(playerMap.containsKey(player.getName())){
+            playerMap.remove(player.getName());
+        }
+        else{
+            throw new IllegalArgumentException("No player of Name " + player.getName()+ " was Found");
+        }
     }
 
     /**
-     * Getter for Inters
+     * Getter for playerMap
      * @return
      */
-    public ArrayList<Player> getInters() {
+    public Set<Player> getInters() {
+        Set<Player> inters = new HashSet<>(playerMap.values());
         return inters;
-    }
-
-    /**
-     * Setter for Inters
-     * @param inters
-     */
-    public void setInters(ArrayList<String> inters) {
-        //You need to abstrac this so this is not an unnecessary type mismatch
-        inters = inters;
     }
 }
--- application/Player.java
+++ application/Player.java
@@ -2,12 +2,25 @@ package application;
 
 public class Player {
     private String name;
+    private int gamesLost;
+    private int gamesWon;
+    private int gamesDodged;
     private String message;
 
     public Player(String inputName, String inputMessage){
         name = inputName;
         message = inputMessage;
+        gamesLost = 0;
+        gamesWon = 0;
+        gamesDodged = 0;
 
+    }
+    public Player(String inputName, int gamesLost, int gamesWon, int gamesDodged, String inputMessage){
+        this.name = inputName;
+        this.message = inputMessage;
+        this.gamesLost = 0;
+        this.gamesWon = 0;
+        this.gamesDodged = 0;
     }
 
     public String getName() {
@@ -24,5 +37,27 @@ public class Player {
 
     public void setMessage(String messageIn) {
         message = messageIn;
+    }
+
+    public int getGamesLost() {
+        return gamesLost;
+    }
+
+    public int getGamesWon() {
+        return gamesWon;
+    }
+
+    public int getGamesLeft() {
+        return gamesDodged;
+    }
+
+    @Override
+    public String toString() {
+        return "Name='" + name + '\'' +
+                ", Games Lost With Player=" + gamesLost +
+                ", Games Won With Player=" + gamesWon +
+                ", Games Dodged With Player=" + gamesDodged +
+                ", Note='" + message + '\'' +
+                '}';
     }
 }
